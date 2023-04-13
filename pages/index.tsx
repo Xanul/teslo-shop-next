6838d41a338ccb0e75ce676ae0a5a62bbfbc9e9c
Xanul <rivas.060@gmail.com>
2023-04-13 16:29:42 -0600

    Added ShopLayout component on /components/layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import { Inter } from 'next/font/google'
+import { Typography } from '@mui/material';
+import { Inter } from 'next/font/google';
+import { ShopLayout } from '../components/layout';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>Teslo Shop</h1>
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Find the best products from Teslo'} >
+      <Typography variant='h1' component='h1'>Home</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>All Products</Typography>
+    </ShopLayout>
   )
 }

60eacca2a80eaa5fefdde26830873cc1180ed293
Xanul <rivas.060@gmail.com>
2023-04-17 18:52:38 -0600

    Added ProductCard and ProductList components on /componentes/products and added the interfaces for products on /interfaces

---
+++ components/products/ProductList.tsx
@@ -0,0 +1,21 @@
+import { Grid } from "@mui/material"
+import { FC } from "react"
+import { IProduct } from '../../interfaces/products';
+import { ProductCard } from './ProductCard';
+
+
+interface Props {
+  products: IProduct[];
+}
+
+export const ProductList: FC<Props> = ({products}) => {
+  return (
+    <Grid container spacing={4}>
+      {
+        products.map(item => (
+          <ProductCard product={item} key={item.slug}/>
+        ))
+      }
+    </Grid>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { Inter } from 'next/font/google';
 import { ShopLayout } from '../components/layout';
 import { initialData } from '../database/products';
+import { ProductList } from '../components/products/ProductList';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,23 +14,9 @@ export default function Home() {
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Find the best products from Teslo'} >
       <Typography variant='h1' component='h1'>Home</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>All Products</Typography>
-      <Grid container spacing={4}>
-        {
-          initialData.products.map( product => (
-            <Grid item xs={6} sm={4} key={product.slug}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia 
-                    component='img'
-                    image={`products/${product.images[0]}`}
-                    alt={product.title}
-                  />
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))
-        }
-      </Grid>
+      <ProductList 
+        products={ initialData.products as any } 
+      />
     </ShopLayout>
   )
 }

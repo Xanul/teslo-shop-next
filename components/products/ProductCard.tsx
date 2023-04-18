0f74660943b44b25d2f1d06ac865a09809641640
Xanul <rivas.060@gmail.com>
2023-04-17 21:16:15 -0600

    Created slug page on /pages/product/slug to handle individual products pages and added NextLink to ProductCard

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,40 +1,52 @@
-import { Grid, Card, CardActionArea, CardMedia, Box, Typography } from "@mui/material";
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Box,
+  Typography,
+  Link,
+} from "@mui/material";
+import NextLink from "next/link";
 import { FC, useMemo, useState } from "react";
-import { IProduct } from '../../interfaces/products';
+import { IProduct } from "../../interfaces/products";
 
 interface Props {
-  product: IProduct
+  product: IProduct;
 }
 
 export const ProductCard: FC<Props> = ({ product }) => {
-
   const [isHovered, setIsHovered] = useState(false);
 
   const productImage = useMemo(() => {
     return isHovered
       ? `products/${product.images[1]}`
-      : `products/${product.images[0]}`
-  }, [isHovered, product.images])
+      : `products/${product.images[0]}`;
+  }, [isHovered, product.images]);
 
   return (
-    <Grid 
-      item xs={6} 
-      sm={4} 
+    <Grid
+      item
+      xs={6}
+      sm={4}
       onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
+      onMouseLeave={() => setIsHovered(false)}>
       <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            className="fadeIn"
-            image={productImage}
-            alt={product.title}
-          />
-        </CardActionArea>
+        <NextLink legacyBehavior href={"/product/slug"} passHref prefetch={false}>
+          <Link>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className="fadeIn"
+                image={productImage}
+                alt={product.title}
+              />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
 
-      <Box sx={{mt:1}} className='fadeIn'>
+      <Box sx={{ mt: 1 }} className="fadeIn">
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={400}>{`$${product.price}`}</Typography>
       </Box>

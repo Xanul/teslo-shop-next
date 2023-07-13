6d9b0c4c983b21ddea69a5d8fa88d451e1967056
Rodrigo Rivas <rivas.060@gmail.com>
2023-07-13 10:35:43 -0600

    Added the feature to show the products in context in the cart page

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -2,14 +2,9 @@ import { initialData } from "@/database/products"
 import { Delete } from "@mui/icons-material";
 import { CardActionArea, CardMedia, Grid, Link, Typography, Box, Button, Divider } from "@mui/material"
 import NextLink from 'next/link';
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { ItemCounter } from "../ui";
-
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2]
-]
+import { CartContext } from "@/context";
 
 interface Props {
   editable?: boolean;
@@ -17,10 +12,14 @@ interface Props {
 
 export const CartList: FC<Props> = ({ editable = false }) => {
 
+  const { cart } = useContext(CartContext);
+  console.log(cart);
+  
+
   return (
     <>
       {
-        productsInCart.map( product => (
+        cart.map( product => (
           <Grid container spacing={2} key={product.slug} sx={{mb:1}}>
             <Grid item xs={3}>
               {/* Link to product page */}
@@ -28,7 +27,7 @@ export const CartList: FC<Props> = ({ editable = false }) => {
                 <Link>
                   <CardActionArea>
                     <CardMedia 
-                      image={`/products/${product.images[0]}`}
+                      image={`/products/${product.image}`}
                       component="img"
                       sx={{borderRadius: '5px'}}
                     />
@@ -39,11 +38,15 @@ export const CartList: FC<Props> = ({ editable = false }) => {
             <Grid item xs={7}>
               <Box display="flex" flexDirection="column">
                 <Typography variant="body1">{product.title}</Typography>
-                <Typography variant="body1">Talla: <strong>M</strong></Typography>
+                <Typography variant="body1">Size: <strong>{product.size}</strong></Typography>
                 {
                   editable
-                  ? <ItemCounter />
-                  : <Typography variant="subtitle1">3 Items</Typography> 
+                  ? <ItemCounter 
+                      currentValue={product.quantity}
+                      maxValue={10}
+                      onUpdateQuantity={() => console.log("e")}
+                    />
+                  : <Typography variant="subtitle1">{product.quantity} {product.quantity > 1 ? 'Items' : 'Item'}</Typography> 
                 }
                 {
                   editable && (

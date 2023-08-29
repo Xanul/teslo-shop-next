307470f455da76b309b935f69fdd91dfe4204a3d
Rodrigo Rivas <rivas.060@gmail.com>
2023-08-28 19:34:50 -0600

    Created the User model and the user interface

---
+++ models/User.ts
@@ -0,0 +1,23 @@
+import { IUser } from '@/interfaces';
+import mongoose, { Schema, Model, model } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    role: {
+        type: String, 
+        enum: {
+            values: ['admin', 'client'],
+            message: '{VALUE} is not a validate role',
+            default: 'client',
+            required: true
+        }
+    }
+}, {
+    timestamps: true
+})
+
+const User:Model<IUser> = mongoose.models.User || model('User', userSchema);
+
+export default User;
